b271cc7d1575fd8fdc1e6728677dc92b27ddef97
deepakjeff2 <95428184+deepakjeff2@users.noreply.github.com>
2021-12-10 20:14:44 +0530

    Update EDA of datasets.py
    categorical treatment

--- EDA of datasets.py
+++ EDA of datasets.py
@@ -63,4 +63,79 @@ class numerical_treatment:
         x = np.where(x>higher,higher,x)
         self.dataframe[column] = pd.DataFrame(x)
         return self.dataframe
+class categorical_imputation(numerical_treatment):
+    def cat_df(self):
+        null_cols = []
+        cat_cols = []
+        class_count=0
+        # to classifing the columns into catogerical columns
+        cat_cols = self.catogerical_columns()
+
+        # to check the number class in the column
+        for column in cat_cols:
+            if self.check_null(column):
+                null_cols.append(column)
+        for column in null_cols:
+            self.dataframe[column] = self.mode_imputation(column)
+        
+        for column in cat_cols:
+            class_count = self.check_class(column)
+            if class_count <2:
+                self.dataframe = self.one_hot_encoding(column)
+            elif (class_count<6) and (class_count>2):
+                self.dataframe = self.label_encoding(column)
+            else:
+                self.dataframe = self.drop(column)
+
+        return self.dataframe
+
+    # to divide categorical columns
+    def catogerical_columns(self):
+        return self.dataframe.select_dtypes(include='object').columns
+
+    # to check whether the column has null values
+    def check_null(self, column):
+        if self.dataframe[column].isnull().sum() > 0:
+            return True
+
+    # to check number of class
+    def check_class(self, column):
+        a = (self.dataframe[column].value_counts().count())
+        return a
+
+
+    # to do mode imputation
+    def mode_imputation(self, column):
+        return self.dataframe[column].fillna(self.dataframe[column].mode()[0])
+
+    # for one hot encoding
+    def one_hot_encoding(self, column):
+        dummy = pd.get_dummies(self.dataframe[column],prefix=column)
+        self.dataframe.drop([column], inplace=True, axis=1)
+        c = pd.concat([self.dataframe, dummy], axis=1, )
+        return c
+
+    # for label_encoding
+    def label_encoding(self,column):
+        labelencoding = LabelEncoder()
+        self.dataframe[column] = labelencoding.fit_transform(self.dataframe[column])
+        return self.dataframe
+
+    # to drop
+    def drop(self,column):
+        self.dataframe = self.dataframe.drop([column],axis=1,inplace=True)
+
+
+
+if __name__=='__main__':
+    #df = pd.read_csv(input("Enter only name of the dataset if it is in the same directory else enter the name with directory:"))
+    #print('Loading...')
+    ob1 = numerical_treatment(df)
+    b=ob1.num_df()
+    ob2 = categorical_imputation(b)
+    a = ob2.cat_df()
+    print(a)
+    #a.to_csv(input('Enter the name of the cleaned dataset to be saved:'))
+    #print("completed")
+
 
