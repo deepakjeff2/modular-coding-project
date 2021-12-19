6bd035b91908f9f8e57ddad4cb231773f7e92af9
deepakjeff2 <95428184+deepakjeff2@users.noreply.github.com>
2021-12-19 21:06:25 +0530

    update

--- Linked list operations.py
+++ Linked list operations.py
@@ -29,6 +29,73 @@ class Linkedlist:
             while n.ref is not None:
                 n = n.ref
             n.ref = new_node
+ def insert_values(self,data_list):
+        self.head = None
+        for data in data_list:
+            self.add_end(data)
+    
+    def count_length(self):
+        count = 0
+        n = self.head
+        while n:
+            count+=1
+            n = n.ref
+        return count
+    
+    def remove_at(self,index):
+        if index < 0 or index >= self.count_length():
+            raise Exception('invalid input')
+        
+        if index == 0:
+            self.head = self.head.ref
+            return
+        
+        count = 0
+        n = self.head
+        while n:
+            if count == index-1:
+                n.ref = n.ref.ref 
+                break
+                
+            n = n.ref
+            count+=1
+
+    def insert_at(self,data,index):
+        if index < 0 or index >= self.count_length():
+            raise Exception('invalid index')
+            
+        if index == 0:
+            self.add_begin(data)
+            return
+        
+        count = 0
+        n = self.head
+        while n:
+            if count == index-1:
+                node = Node(data)
+                node.ref = n.ref
+                n.ref = node
+            n = n.ref
+            count+=1
+
+    def inser_after(self,data_after,data):
+        n = self.head
+        while n:
+            if data_after == n.data:
+                node = Node(data)
+                node.ref = n.ref
+                n.ref = node
+                break
+                
+            n = n.ref
+    
+    def remove_by_value(self,data):
+        n = self.head
+        while n:
+            if data == n.ref.data:
+                n.ref = n.ref.ref
+                break
+            n = n.ref
             
             
 if __name__ == '__main__':       
